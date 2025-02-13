4217eb45d96eb85b4bf1d60bf5cd015651b818c7
ortoct <104336410+ortoct@users.noreply.github.com>
2025-02-13 14:05:48 -0800

    Updated HexToDec

--- ExceptionHandling/ConvertHexToDec.java
+++ ExceptionHandling/ConvertHexToDec.java
@@ -5,9 +5,15 @@ public class ConvertHexToDec
 {
    public static void main(String[] args)
    {     
-      String hex = readHexFromFile( "HexNum.txt" );
-      int num = convertHexToDec( hex );
-      System.out.println( "Hex Value: " + hex + "\nDec Value: " + num );
+      try
+      {
+         String hex = readHexFromFile( "HexNum.txt" );
+         int num = convertHexToDec( "2GD" );
+         System.out.println( "Hex Value: " + hex + "\nDec Value: " + num );
+      }catch( NumberFormatException e )
+      {
+         System.out.println( e.getMessage() );
+      }
    }
    
    public static String readHexFromFile( String filename )
@@ -27,6 +33,21 @@ public class ConvertHexToDec
    
    public static int convertHexToDec( String hexVal ) throws NumberFormatException
    {
-      return 701;
+      int dec = 0; int exp = 0;
+      for(int i = hexVal.length()-1; i >= 0; i--)
+      {
+         char digit = hexVal.charAt(i);
+         int num;
+         if( digit >= 48 && digit <= 57 )
+            num = digit - 48;
+         else if( digit >= 65 && digit <= 70 )
+            num = digit - 55;
+         else
+            throw new NumberFormatException("Invalid Hex Character");
+         
+         dec += num*Math.pow(16,exp);
+         exp++;
+      }
+      return dec;
    }
 }
