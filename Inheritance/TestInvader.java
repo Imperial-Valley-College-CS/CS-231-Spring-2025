4d0dd024471fdbbcc8bd67488e5b59f3ac8d8ac7
ortoct <104336410+ortoct@users.noreply.github.com>
2025-02-18 13:43:09 -0800

    started setBody method

--- Inheritance/TestInvader.java
+++ Inheritance/TestInvader.java
@@ -1,7 +1,30 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class TestInvader
 {
    public static void main(String[] args)
    {
       Squid squid = new Squid(10,50);
+      setBody("InvaderMatrices/CrabMatrix.txt");
+   }
+   
+   public static boolean[][] setBody( String filename )
+   {
+      File f = new File( filename );
+      try
+      {
+         Scanner scan = new Scanner(f);
+         
+         boolean[][] body = new boolean[8][12];
+         for( int i = 0; i < body.length; i++ )
+         {         
+            System.out.println( scan.nextLine() );
+         }
+         return null;
+      }catch(FileNotFoundException e)
+      {
+         return null;
+      }
    }
 }
