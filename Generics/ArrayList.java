a11fc5fb41f66b9d87a37bee6a367c8e9607854e
ortoct <104336410+ortoct@users.noreply.github.com>
2025-03-27 13:43:05 -0700

    Updated ArrayList to implement Iterable

--- Generics/ArrayList.java
+++ Generics/ArrayList.java
@@ -1,48 +1,106 @@
-public class ArrayList<E>
+import java.util.Iterator;
+
+public class ArrayList<E> implements Iterable
 {
-   //create an array to store all elements of arraylist
-   private E[] arr;
-   private int size = 0;
+   E[] arr;
+   int size = 0;
    
-   //constructor
    public ArrayList()
    {
-      this.arr = (E[])(new Object[2]);   //default initial size of array
+      arr = (E[])(new Object[2]);      //initial number of elements
    }
    
-   public ArrayList(int size)
+   public void add( E obj )
    {
-      this.arr = (E[])(new Object[size]);   //user-defined initial size of array
+      if( size == arr.length )
+         resize();
+      arr[size] = obj;
+      size++;
    }
    
-   public void add(E obj)
+   public void add( int index, E obj )
    {
-      if( this.size == arr.length )    //check if arr is full
+      if( size == arr.length )
          resize();
          
-      this.arr[this.size] = obj;
-      this.size++;
+      for( int i = size; i > index; i-- )
+         arr[i] = arr[i-1];
+      
+      arr[index] = obj;
+      size++;
    }
    
-   public void add(E obj, int index)
+   public E remove( int index )
    {
-      if( this.size == arr.length )    //check if arr is full
-         resize();
+      E elem = arr[index];    //store element to be removed and returned
+      size--;
+      
+      for( int i = index; i < size; i++ )    //shift elements 1 space to the left
+         arr[i] = arr[i+1];
          
-      for( int i = size; i > index; i-- )  //shifts elements one space to left
-         this.arr[i] = arr[i-1];
-         
-      this.arr[index] = obj;     //insert element at specified index
-      this.size++;               
+      arr[size] = null;    //set last element in list to null
+      return elem;
    }
    
-   private void resize()
+   public E get( int index )
    {
-      E[] newArr = (E[])(new Object[arr.length*2]);
-      for(int i = 0; i < this.size; i++)
+      if( index >= size )
+         throw new ArrayIndexOutOfBoundsException();
+         
+      return arr[index];
+   }
+   
+   private void resize()      //called when arr is full
+   {
+      E[] newArr = arr;                   //newArr has all elements in arr
+      arr = (E[])(new Object[2*size]);    //lose all the elements in arr
+      
+      for( int i = 0; i < size; i++ )
+         arr[i] = newArr[i];              //copy over elements from newArr into arr
+   }
+   
+   @Override
+   public Iterator<E> iterator()
+   {
+      return new ListIterator<E>(this);
+   }
+   
+   class ListIterator<E> implements Iterator<E>
+   {
+      int index = 0;
+      E elem;
+      ArrayList<E> list;
+      
+      public ListIterator(ArrayList<E> list)
       {
-         newArr[i] = this.arr[i];
+         this.list = list;
+         elem = this.list.get(index);
       }
-      this.arr = newArr;
+      
+      @Override
+      public boolean hasNext()
+      {
+         try
+         {
+            E next = this.list.get(index);
+            if( next != null )
+               return true;
+               
+            return false;
+         }catch( ArrayIndexOutOfBoundsException e )
+         {
+            return false;
+         }
+         
+      }
+      
+      @Override
+      public E next()
+      {
+         E current = this.list.get(index);
+         index++;
+         return current;
+      }
+   
    }
 }
