67467843219d6e16d800d7dfb1a28f964573178e
ortoct <104336410+ortoct@users.noreply.github.com>
2025-05-01 13:38:00 -0700

    Added Canvas

--- JavaFX/Solitaire/App.java
+++ JavaFX/Solitaire/App.java
@@ -3,9 +3,18 @@ import javafx.stage.Stage;
 
 public class App extends Application
 {
+   Group g = new Group();     //no argument necessary
+   Scene scene = new Scene(g); //Parent argument required
+   Canvas canvas = new Canvas(400,600);
+   GraphicsContext gc = canvas.getGraphicsContext2D();
+   
    @Override
    public void start(Stage s)
    {
+      g.getChildren().add( canvas );
+      gc.setFill( Color.RED );
+      gc.fillRect(50,50,10,10);
+      Card c = new Card("2","C");
       s.show();
    }
 }
